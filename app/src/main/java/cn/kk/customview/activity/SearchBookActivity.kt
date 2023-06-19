9f865647c51b22ba8b0bbcffde3303927563c1b5
kamaihamaiha <kamaihamaiha@gmail.com>
2023-06-20 00:03:03 +0800

    优化搜索显示

--- app/src/main/java/cn/kk/customview/activity/SearchBookActivity.kt
+++ app/src/main/java/cn/kk/customview/activity/SearchBookActivity.kt
@@ -1,13 +1,21 @@
 package cn.kk.customview.activity
 
+import android.content.Context
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import cn.kk.base.UIHelper
 import cn.kk.base.activity.BaseActivity
 import cn.kk.base.bean.BaseItem
 import cn.kk.base.utils.AssetsHelper
 import cn.kk.customview.R
+import cn.kk.customview.activity.book.BookDetailActivity
 import cn.kk.customview.factory.BookModelFactory
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
@@ -16,6 +24,10 @@ import kotlinx.android.synthetic.main.activity_search_book.*
 class SearchBookActivity: BaseActivity() {
     override fun getLayout(): Int {
         return R.layout.activity_search_book
+    }
+
+    private val mEmptyView by lazy {
+        LayoutInflater.from(this@SearchBookActivity).inflate(R.layout.item_list_empty, null)
     }
 
     private val mBookList by lazy {
@@ -39,13 +51,13 @@ class SearchBookActivity: BaseActivity() {
                 holder.setText(
                     R.id.tv_type, when (item.type) {
                         BaseItem.Type.TYPE_BOOK -> {
-                            "book"
+                            "书"
                         }
                         BaseItem.Type.TYPE_CHAPTER -> {
-                            "chapter"
+                            "章节"
                         }
                         BaseItem.Type.TYPE_SECTION -> {
-                            "section"
+                            "小节"
                         }
                         else -> {
                             "unknown"
@@ -53,10 +65,30 @@ class SearchBookActivity: BaseActivity() {
                     }
                 )
             }
+        }.apply {
+            setEmptyView(mEmptyView)
+            setOnItemClickListener { adapter, view, position ->
+                // todo open book
+                UIHelper.toast(data[position].title, this@SearchBookActivity)
+//                openNextUI(BookDetailActivity::class.java, data[position].title, data[position])
+            }
         }
 
         btn_cancel.setOnClickListener { finish() }
 
+        et_search.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (event?.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    // 在这里执行收起键盘的操作
+                    val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v!!.windowToken, 0)
+
+                    return true
+                }
+                return false
+            }
+
+        })
         et_search.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -76,10 +108,11 @@ class SearchBookActivity: BaseActivity() {
 
                     }
                 }
-                if (TextUtils.isEmpty(s))
 
                 // refresh ui
+                tv_result_count.text = "共${mSearchKeywordResultList.size}条结果"
                 (rv_result?.adapter as BaseQuickAdapter<BaseItem, BaseViewHolder>)?.notifyDataSetChanged()
+//                changeEmptyViewState()
             }
         })
 
@@ -87,7 +120,7 @@ class SearchBookActivity: BaseActivity() {
     }
 
 
-    fun getBookKeywords(): MutableList<BaseItem> {
+   private fun getBookKeywords(): MutableList<BaseItem> {
         val bookKeywords = mutableListOf<BaseItem>()
         mBookList.forEach {
             bookKeywords.add(it.apply { type = BaseItem.Type.TYPE_BOOK })
