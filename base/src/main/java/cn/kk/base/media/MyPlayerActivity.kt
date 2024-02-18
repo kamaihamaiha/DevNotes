ec0912d59cb60f4f43ecf6b3500b22778ac6b1d6
kk <kamaihamaiha@gmail.com>
2024-02-18 18:34:13 +0800

    播放效果调整

--- base/src/main/java/cn/kk/base/media/MyPlayerActivity.kt
+++ base/src/main/java/cn/kk/base/media/MyPlayerActivity.kt
@@ -1,5 +1,10 @@
 package cn.kk.base.media
 
+import android.view.View
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.media3.common.MediaItem
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultHttpDataSource
@@ -8,6 +13,7 @@ import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.ui.PlayerView
 import cn.kk.base.R
+import cn.kk.base.UIHelper
 import cn.kk.base.activity.BaseActivity
 
 class MyPlayerActivity: BaseActivity() {
@@ -24,9 +30,38 @@ class MyPlayerActivity: BaseActivity() {
     override fun doWhenOnCreate() {
         super.doWhenOnCreate()
 
-
+        UIHelper.setStatusBarDark(this@MyPlayerActivity)
+        val rootView = findViewById<View>(R.id.rootView)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, object :
+            OnApplyWindowInsetsListener {
+            override fun onApplyWindowInsets(
+                v: View,
+                windowInsets: WindowInsetsCompat
+            ): WindowInsetsCompat {
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.updatePadding(top = insets.top)
+                return WindowInsetsCompat.CONSUMED
+            }
+        })
         initPlayer()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (myExoPlayer.isPlaying) {
+            myExoPlayer.pause()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myExoPlayer.stop()
+        myExoPlayer.release()
     }
 
     private fun initPlayer(){
