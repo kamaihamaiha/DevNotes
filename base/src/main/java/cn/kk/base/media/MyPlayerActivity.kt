c48af4ddd864f67c7baaf7bbe8307c5457a071bc
kk <kamaihamaiha@gmail.com>
2024-07-07 22:53:57 +0800

    支持蓝牙遥控器的按键控制播放

--- base/src/main/java/cn/kk/base/media/MyPlayerActivity.kt
+++ base/src/main/java/cn/kk/base/media/MyPlayerActivity.kt
@@ -1,5 +1,7 @@
 package cn.kk.base.media
 
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
@@ -13,7 +15,6 @@ import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.MediaSource
-import androidx.media3.ui.PlayerView
 import cn.kk.base.R
 import cn.kk.base.UIHelper
 import cn.kk.base.activity.BaseActivity
@@ -63,6 +64,113 @@ class MyPlayerActivity: BaseActivity() {
 
         initPlayer()
 
+    }
+
+    var moveTag = false // 上下左右 按键就是true，点击事件就是 false
+    var actionTag = -1
+    val click_tag = 0
+    val left_tag = 1
+    val right_tag = 2
+    val up_tag = 3
+    val down_tag = 4
+
+    var startX = 0
+    var startY = 0
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        ev?.action?.let {
+            when(it){
+                MotionEvent.ACTION_DOWN -> {
+                    moveTag = false
+                    startX = ev.x.toInt()
+                    startY = ev.y.toInt()
+                }
+                MotionEvent.ACTION_UP -> {
+                    if (!moveTag) {
+                        actionTag = click_tag
+                    }
+                    startControlPlay()
+                    moveTag = false
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    moveTag = true
+                    // 判断移动方向
+                    if (ev.y.toInt() == startY) {
+                        // 水平移动: x越来越小
+                        if (ev.x.toInt() < startX) {
+                            actionTag = right_tag
+                        } else if (ev.x.toInt() > startX) {
+                            actionTag = left_tag
+                        }
+
+                    } else if (ev.x.toInt() == startX) {
+                        // 垂直移动
+                        if (ev.y.toInt() < startY) {
+                            actionTag = down_tag
+                        } else if (ev.y.toInt() > startY) {
+                            actionTag = up_tag
+                        }
+                    }
+
+                }
+                else -> {
+
+                }
+            }
+        }
+//        return true
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun getActionMsg(action: Int): String {
+        return when(action){
+            click_tag -> "play/pause"
+            left_tag -> "left"
+            right_tag -> "right"
+            up_tag -> "up"
+            down_tag -> "down"
+            else -> "unknown"
+        }
+    }
+
+    private fun startControlPlay(){
+        when(actionTag){
+            click_tag -> {
+                if (myExoPlayer.isPlaying) {
+                    myExoPlayer.pause()
+                } else {
+                    myExoPlayer.play()
+                }
+            }
+            left_tag -> {
+                myExoPlayer.seekTo(myExoPlayer.currentPosition - 10000)
+            }
+            right_tag -> {
+                myExoPlayer.seekTo(myExoPlayer.currentPosition + 10000)
+            }
+            up_tag -> {
+                myExoPlayer.seekTo(myExoPlayer.currentPosition + 60000)
+            }
+            down_tag -> {
+                myExoPlayer.seekTo(myExoPlayer.currentPosition - 60000)
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
+
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        return super.dispatchKeyEvent(event)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return super.onKeyDown(keyCode, event)
     }
 
     override fun onResume() {
